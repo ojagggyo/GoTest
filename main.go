0ac7b8c0f8c6b2fa24f42b2ddba3e564e0ec9aac
ojagggyo <42034667+ojagggyo@users.noreply.github.com>
2026-06-27 20:25:01 +0900

    Create main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, Go Server!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	fmt.Println("Server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
