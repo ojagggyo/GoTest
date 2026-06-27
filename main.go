55e24209d432a853e8f887546b06b0a53caa59e4
ojagggyo <ojagggyo@gmail.com>
2026-06-27 22:04:17 +0900

    m

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"net/http"
 )
 
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello")
+}
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, Go Server!")
+func about(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "About page")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/about", about)
 
 	fmt.Println("Server running at http://localhost:8111")
 	http.ListenAndServe(":8111", nil)
