6bca5fae6de225c685df6beefd60a02e57d15b60
ojagggyo <ojagggyo@gmail.com>
2026-06-27 21:33:38 +0900

    m

--- main.go
+++ main.go
@@ -13,6 +13,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", handler)
 
-	fmt.Println("Server running at http://localhost:8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("Server running at http://localhost:8111")
+	http.ListenAndServe(":8111", nil)
 }
